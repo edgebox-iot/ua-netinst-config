82d287dfabaca2b0df75efbdeea2460b32aa3f66
Paulo Truta <pinheirotruta5@gmail.com>
2023-03-26 19:36:39 +0200

    Added edgeboxsetup.service at level3

--- config/files/root/home/system/edgebox.sh
+++ config/files/root/home/system/edgebox.sh
@@ -43,7 +43,7 @@ install_cloudflared() {
     echo "--> Installing cloudflared"
     echo ""
     wget -q https://github.com/cloudflare/cloudflared/releases/download/2023.3.1/cloudflared-linux-arm.deb
-    dpkg -i cloudflared-linux-arm.deb
+    sudo dpkg -i cloudflared-linux-arm.deb
 }
 
 
@@ -93,102 +93,111 @@ while [ $# -gt 0 ] ; do
         ;;
     -s|--setup)
         setup=1
-        key_name="github_key"
-        pubkey_name="github_key.pub"
-        key_found=0
-        for f in home/system/.ssh/ ; do
-            FILE="$key_name"
-            if test -f "$FILE"; then
-                key_found=1
-                echo "Setting up GitHub SSH key in home/system/.ssh/$FILE"
-                eval "$(ssh-agent -s)"
-                ssh-add home/system/.ssh/github_key
-            fi
-        done
-        echo ""
-        echo "--> Initializing Edgebox Setup Script"
-        echo ""
-        install_buster_backports
-        echo "----> Installing Docker:"
-        echo ""
-        curl -ksSL https://get.docker.com | sh
-        echo ""
-        echo "----> Installing Docker Compose:"
-        echo ""
-        sudo pip3 -v install docker-compose
-        echo ""
-        echo ""
-        echo "----> Settting up edgebox-iot/ws"
-        echo ""
-        mkdir /home/system/components
-        git config --global credential.helper cache # Set git to use the credential memory cache
-        git config --global credential.helper 'cache --timeout=3600' # Set the cache to timeout after 1 hour (setting is in seconds)
-        cd /home/system/components
-        if [ $key_found != 0 ]; then
-            git clone git@github.com:edgebox-iot/ws.git
+        if [ -e /home/system/components/.installed ]; then
+            echo "File exists! Skipping Components Installation"
         else
-            git clone https://github.com/edgebox-iot/ws.git
-        fi
-        chmod 757 ws
-        mkdir appdata
-        touch /home/system/components/ws/assets/installing.html
-        sudo chmod -R 777 appdata/
-        ./ws -b
-        echo "----> Installing yq:"
-        echo ""
-        sudo pip3 -v install yq
-        echo ""
-        echo "----> Installing Go:"
-        export GOLANG="$(curl https://go.dev/dl/|grep armv6l|grep -v beta|head -1|awk -F\> {'print $3'}|awk -F\< {'print $1'})"
-        wget https://golang.org/dl/$GOLANG
-        sudo tar -C /usr/local -xzf $GOLANG
-        rm $GOLANG
-        unset GOLANG
-        echo "" >> /home/system/.profile
-        echo "export PATH=\$PATH:/usr/local/go/bin" >> /home/system/.profile
-        echo ""
-        echo "----> Setting up edgebox-iot/edgeboxctl"
-        echo ""
-        if [ $key_found != 0 ]; then
-            git clone git@github.com:edgebox-iot/edgeboxctl.git
-        else
-            git clone https://github.com/edgebox-iot/edgeboxctl.git
-	    fi
-        echo ""
-        echo "----> Settting up edgebox-iot/api"
-        echo ""
-        if [ $key_found != 0 ]; then
-            git clone git@github.com:edgebox-iot/api.git
-        else
-            git clone https://github.com/edgebox-iot/api.git
-        fi
+            echo "File does not exist. Installing component dependencies"
+            key_name="github_key"
+            pubkey_name="github_key.pub"
+            key_found=0
+            for f in home/system/.ssh/ ; do
+                FILE="$key_name"
+                if test -f "$FILE"; then
+                    key_found=1
+                    echo "Setting up GitHub SSH key in home/system/.ssh/$FILE"
+                    eval "$(ssh-agent -s)"
+                    ssh-add home/system/.ssh/github_key
+                fi
+            done
             echo ""
-        echo "----> Setting up edgebox-iot/apps"
-        if [ $key_found != 0 ]; then
-            git clone git@github.com:edgebox-iot/apps.git
-        else
-            git clone https://github.com/edgebox-iot/apps.git
+            echo "--> Initializing Edgebox Setup Script"
+            echo ""
+            install_buster_backports
+            echo "----> Installing Docker:"
+            echo ""
+            curl -ksSL https://get.docker.com | sh
+            echo ""
+            echo "----> Installing Docker Compose:"
+            echo ""
+            sudo pip3 -v install docker-compose
+            echo ""
+            echo ""
+            echo "----> Settting up edgebox-iot/ws"
+            echo ""
+            mkdir /home/system/components
+            git config --global credential.helper cache # Set git to use the credential memory cache
+            git config --global credential.helper 'cache --timeout=3600' # Set the cache to timeout after 1 hour (setting is in seconds)
+            cd /home/system/components
+            if [ $key_found != 0 ]; then
+                git clone git@github.com:edgebox-iot/ws.git
+            else
+                git clone https://github.com/edgebox-iot/ws.git
+            fi
+            chmod 757 ws
+            cd ws
+            mkdir appdata
+            touch /home/system/components/ws/assets/installing.html
+            sudo chmod -R 777 appdata/
+            ./ws -b
+            cd ..
+            echo "----> Installing yq:"
+            echo ""
+            sudo pip3 -v install yq
+            echo ""
+            echo "----> Installing Go:"
+            export GOLANG="$(curl https://go.dev/dl/|grep armv6l|grep -v beta|head -1|awk -F\> {'print $3'}|awk -F\< {'print $1'})"
+            wget https://golang.org/dl/$GOLANG
+            sudo tar -C /usr/local -xzf $GOLANG
+            rm $GOLANG
+            unset GOLANG
+            echo "" >> /home/system/.profile
+            echo "export PATH=\$PATH:/usr/local/go/bin" >> /home/system/.profile
+            export PATH=$PATH:/usr/local/go/bin
+            echo ""
+            echo "----> Setting up edgebox-iot/edgeboxctl"
+            echo ""
+            if [ $key_found != 0 ]; then
+                git clone git@github.com:edgebox-iot/edgeboxctl.git
+            else
+                git clone https://github.com/edgebox-iot/edgeboxctl.git
+            fi
+            echo ""
+            echo "----> Settting up edgebox-iot/api"
+            echo ""
+            if [ $key_found != 0 ]; then
+                git clone git@github.com:edgebox-iot/api.git
+            else
+                git clone https://github.com/edgebox-iot/api.git
+            fi
+                echo ""
+            echo "----> Setting up edgebox-iot/apps"
+            if [ $key_found != 0 ]; then
+                git clone git@github.com:edgebox-iot/apps.git
+            else
+                git clone https://github.com/edgebox-iot/apps.git
+            fi
+            echo ""
+            echo "----> Building Reverse Proxy and Service Containers Configs"
+            echo ""
+            cd ws
+            ./ws -b
+            echo ""
+            echo "----> Installing edgeboxctl"
+            echo ""
+            install_edgeboxctl
+            install_cloudflared
+            echo ""
+            echo "----> Starting Edgeboxctl"
+            sudo systemctl enable edgeboxctl
+            sudo systemctl start edgeboxctl
+            rm /home/system/components/ws/assets/installing.html
+            touch /home/system/components/.installed
+            echo ""
+            echo "---------------------------"
+            echo "| Edgebox Setup Finished  |"
+            echo "---------------------------"
+            echo ""
         fi
-        echo ""
-        echo "----> Building Reverse Proxy and Service Containers Configs"
-        echo ""
-        cd ws
-        ./ws -b
-        echo ""
-        echo "----> Installing edgeboxctl"
-        echo ""
-        install_edgeboxctl
-        install_cloudflared
-        echo ""
-        echo "----> Starting Edgeboxctl"
-        sudo systemctl enable edgeboxctl
-        sudo systemctl start edgeboxctl
-        rm /home/system/components/ws/assets/installing.html
-        echo ""
-        echo "---------------------------"
-        echo "| Edgebox Setup Finished  |"
-        echo "---------------------------"
-        echo ""
         ;;
     -o|--output)
         output="$2"
